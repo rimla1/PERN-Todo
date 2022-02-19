81095372aee39666b48cf35a6f1126e224ec0aa4
Almir Muminovic <projectalmir@gmail.com>
2022-02-19 11:24:07 +0100

    Added update a todo middleware function

--- server/app.js
+++ server/app.js
@@ -53,6 +53,17 @@ app.get('/todos/:id', async (req, res, next) => {
 
 //update a todo
 
+app.put("/todos/:id", async (req, res, next) => {
+    try {
+        const { id } = req.params
+        const { description } = req.body;
+        const updateTodo = await pool.query("UPDATE todo SET description = $1 WHERE todo_id = $2", [description, id]);
+        res.json("Todo was updated!")
+    } catch (err) {
+        console.error(err.message)
+    }
+})
+
 //delete a todo
 
 app.listen(5000, () => {
