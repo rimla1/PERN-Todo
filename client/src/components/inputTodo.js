b8252a48390a3d2082da5a21856494e101656414
Almir Muminovic <projectalmir@gmail.com>
2022-02-22 15:22:56 +0100

    Adding react hook useState, and set default value to Hello

--- client/src/components/inputTodo.js
+++ client/src/components/inputTodo.js
@@ -1,11 +1,13 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 
 const InputTodo = () => {
+    const [desc, setDesc] = useState("Hello");
+
     return (
         <Fragment>
             <h1 className="text-center mt-5">Pern Todo List</h1>
             <form className="d-flex mt-5">
-                <input type="text" className="form-control"/>
+                <input type="text" className="form-control" value={desc}/>
                 <button className="btn btn-success">Add</button>
             </form>
         </Fragment>
