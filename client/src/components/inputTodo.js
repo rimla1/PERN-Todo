e8a7f6f25d4ee2a7919d80018d7b9b656dc55e75
Almir Muminovic <projectalmir@gmail.com>
2022-02-23 21:59:30 +0100

    Give onChange to input so we can edit input value, instead of having default value

--- client/src/components/inputTodo.js
+++ client/src/components/inputTodo.js
@@ -7,7 +7,7 @@ const InputTodo = () => {
         <Fragment>
             <h1 className="text-center mt-5">Pern Todo List</h1>
             <form className="d-flex mt-5">
-                <input type="text" className="form-control" value={desc}/>
+                <input type="text" className="form-control" value={desc} onChange={e => setDesc(e.target.value)}/>
                 <button className="btn btn-success">Add</button>
             </form>
         </Fragment>
