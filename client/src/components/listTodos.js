078c66f3043176dab70305e0c86e7a79bbbc91ed
Almir Muminovic <projectalmir@gmail.com>
2022-02-24 16:46:22 +0100

    Adding basic table strcuture into listTodo without any functionality

--- client/src/components/listTodos.js
+++ client/src/components/listTodos.js
@@ -1,8 +1,35 @@
-import react from "react";
+import react, { Fragment } from "react";
 
 const ListTodo = () => {
     return (
-        <h1> List Todos </h1>
+        <Fragment>
+             <table className="table">
+    <thead>
+      <tr>
+        <th>Firstname</th>
+        <th>Lastname</th>
+        <th>Email</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>John</td>
+        <td>Doe</td>
+        <td>john@example.com</td>
+      </tr>
+      <tr>
+        <td>Mary</td>
+        <td>Moe</td>
+        <td>mary@example.com</td>
+      </tr>
+      <tr>
+        <td>July</td>
+        <td>Dooley</td>
+        <td>july@example.com</td>
+      </tr>
+    </tbody>
+  </table>
+        </Fragment>
     )
 }
 
