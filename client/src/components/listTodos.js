92c14d8e540c1427ae18ca38131e37fdb12c04e3
Almir Muminovic <projectalmir@gmail.com>
2022-02-25 14:37:33 +0100

    Finishing our list todo component, displaying data from database to our react page

--- client/src/components/listTodos.js
+++ client/src/components/listTodos.js
@@ -1,34 +1,50 @@
-import react, { Fragment } from "react";
+import react, { Fragment, useEffect, useState } from "react";
 
 const ListTodo = () => {
+
+    const [todos, setTodos] = useState([])
+
+    const getTodos = async () => {
+
+        try {
+            const response = await fetch("http://localhost:5000/todos")
+            const jsonData = await response.json()
+
+            setTodos(jsonData)
+        } catch (err) {
+            console.error(err.message)
+        }
+    }
+
+    useEffect(() => {
+        getTodos()
+    }, [])
+
+    console.log(todos)
+
     return (
         <Fragment>
-             <table className="table">
-    <thead>
-      <tr>
-        <th>Firstname</th>
-        <th>Lastname</th>
-        <th>Email</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>John</td>
-        <td>Doe</td>
-        <td>john@example.com</td>
-      </tr>
-      <tr>
-        <td>Mary</td>
-        <td>Moe</td>
-        <td>mary@example.com</td>
-      </tr>
-      <tr>
-        <td>July</td>
-        <td>Dooley</td>
-        <td>july@example.com</td>
-      </tr>
-    </tbody>
-  </table>
+                {" "}
+                <table className="table mt-5 text-center">
+                    <thead>
+                        <tr>
+                            <th>Description</th>
+                            <th>Edit</th>
+                            <th>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            todos.map(todo => (
+                                <tr>
+                                    <td>{todo.description}</td>
+                                    <td>Edit</td>
+                                    <td>Delete</td>
+                                </tr>
+                            ))
+                        }
+                    </tbody>
+                </table>
         </Fragment>
     )
 }
