c59f83fec1e80505258c1b1b72603aa18201c9fb
Almir Muminovic <projectalmir@gmail.com>
2022-02-25 14:52:05 +0100

    Deleting our todo from database, need to get rid of it from our react app without refreshing it

--- client/src/components/listTodos.js
+++ client/src/components/listTodos.js
@@ -4,6 +4,21 @@ const ListTodo = () => {
 
     const [todos, setTodos] = useState([])
 
+    // Delete Function
+
+    const deleteTodo = async (id) => {
+        try {
+            const deleteTodo = await fetch(`http://localhost:5000/todos/${id}`, {
+                method: "DELETE"
+            });
+
+            console.log(deleteTodo)
+
+        } catch (err) {
+            console.error(err.message)
+        }
+    }
+
     const getTodos = async () => {
 
         try {
@@ -36,10 +51,10 @@ const ListTodo = () => {
                     <tbody>
                         {
                             todos.map(todo => (
-                                <tr>
+                                <tr key={todo.todo_id}>
                                     <td>{todo.description}</td>
                                     <td>Edit</td>
-                                    <td>Delete</td>
+                                    <td><button className="btn btn-danger" onClick={() => deleteTodo(todo.todo_id)}>Delete</button></td>
                                 </tr>
                             ))
                         }
