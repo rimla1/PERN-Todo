f2bb1916b444c9179c436e2188b62cb548703a80
Almir Muminovic <projectalmir@gmail.com>
2022-02-26 15:04:40 +0100

    Fixing error: instead of class we using className

--- client/src/components/editTodo.js
+++ client/src/components/editTodo.js
@@ -1,27 +1,29 @@
 import React, { Fragment } from 'react';
 
-const EditTodo = () => {
+const EditTodo = ({ todo }) => {
+    console.log(todo)
+
     return(
     <Fragment >
 
-        <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        <button type="button" className="btn btn-warning" data-bs-toggle="modal" data-bs-target="#exampleModal">
         Edit
         </button>
 
 
-        <div class="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">Edit Todo</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal-dialog">
+            <div className="modal-content">
+            <div className="modal-header">
+                <h5 className="modal-title" id="exampleModalLabel">Edit Todo</h5>
+                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <div class="modal-body">
+            <div className="modal-body">
                 <input type="text" className="form-control" />
             </div>
-            <div class="modal-footer">
-                <button type="button" class="btn btn-warning" data-bs-dismiss="modal">Edit</button>
-                <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
+            <div className="modal-footer">
+                <button type="button" className="btn btn-warning" data-bs-dismiss="modal">Edit</button>
+                <button type="button" className="btn btn-danger" data-bs-dismiss="modal">Close</button>
             </div>
             </div>
         </div>
