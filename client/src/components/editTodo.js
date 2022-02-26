f6b9afccaab97ebe1e7f9238434420a5aff3eec3
Almir Muminovic <projectalmir@gmail.com>
2022-02-26 16:36:29 +0100

    Finishing our project

--- client/src/components/editTodo.js
+++ client/src/components/editTodo.js
@@ -29,19 +29,25 @@ const EditTodo = ({ todo }) => {
         </button>
 
 
-        <div className="modal fade" id={`id${todo.todo_id}`} tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal fade" id={`id${todo.todo_id}`} tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" onClick={() => 
+                    setDescription(todo.description)
+                }>
         <div className="modal-dialog">
             <div className="modal-content">
             <div className="modal-header">
                 <h5 className="modal-title" id="exampleModalLabel">Edit Todo</h5>
-                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" onClick={() => 
+                    setDescription(todo.description)
+                }></button>
             </div>
             <div className="modal-body">
                 <input type="text" className="form-control" value={description} onChange={e => setDescription(e.target.value)}/>
             </div>
             <div className="modal-footer">
                 <button type="button" className="btn btn-warning" data-bs-dismiss="modal" onClick={e => updateDescription(e)}>Edit</button>
-                <button type="button" className="btn btn-danger" data-bs-dismiss="modal">Close</button>
+                <button type="button" className="btn btn-danger" data-bs-dismiss="modal" onClick={() => {
+                    setDescription(todo.description)
+                }}>Close</button>
             </div>
             </div>
         </div>
