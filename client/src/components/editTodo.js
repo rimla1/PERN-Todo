2fb2db1eeffa3bf17cb810cb34fff0b517dc1647
Almir Muminovic <projectalmir@gmail.com>
2022-02-26 15:12:29 +0100

    On edit we see individual specific value for all todos we want to edit

--- client/src/components/editTodo.js
+++ client/src/components/editTodo.js
@@ -1,17 +1,18 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 const EditTodo = ({ todo }) => {
-    console.log(todo)
+    
+    const [description, setDescription] = useState(todo.description)
 
     return(
     <Fragment >
 
-        <button type="button" className="btn btn-warning" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        <button type="button" className="btn btn-warning" data-bs-toggle="modal" data-bs-target={`#id${todo.todo_id}`}>
         Edit
         </button>
 
 
-        <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal fade" id={`id${todo.todo_id}`} tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div className="modal-dialog">
             <div className="modal-content">
             <div className="modal-header">
@@ -19,10 +20,10 @@ const EditTodo = ({ todo }) => {
                 <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div className="modal-body">
-                <input type="text" className="form-control" />
+                <input type="text" className="form-control" value={description}/>
             </div>
             <div className="modal-footer">
-                <button type="button" className="btn btn-warning" data-bs-dismiss="modal">Edit</button>
+                <button type="button" className="btn btn-warning" data-bs-dismiss="modal" onClick={setDescription}>Edit</button>
                 <button type="button" className="btn btn-danger" data-bs-dismiss="modal">Close</button>
             </div>
             </div>
